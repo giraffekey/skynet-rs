f26e466eeb514baf1e6ad20288b34d13f1a78f20
Luuk de Waal Malefijt <luuk@waalmalefijt.nl>
2022-06-21 12:06:39 +0200

    impl TUS support

--- src/upload.rs
+++ src/upload.rs
@@ -11,6 +11,24 @@ use mime::Mime;
 use serde::Deserialize;
 use textnonce::TextNonce;
 use walkdir::WalkDir;
+use tus_async_client::{Client, HttpHandler};
+use reqwest::{self, ClientBuilder};
+use std::rc::Rc;
+use http::Uri;
+use crate::util::make_reqwest_headers;
+
+/// Skynet uploads data in chunks.
+/// The size of these chunks depends on erasure coding settings specified for the fanout and the specified encryption type.
+/// The formula for the size of these chunks is chunkSize := (4MiB — encryptionOverhead) * fanoutDataPieces.
+/// By default, data uploaded to Skynet uses 10 data pieces for its fanout and Threefish for encryption which doesn’t have any overhead.
+/// As a result, the default chunk size is 40MiB. Since portals have limited amounts of RAM,
+/// they can’t keep these chunks in memory while waiting for users to resume their uploads.
+/// That’s why the chunk size specified in TUS needs to be a multiple of the Skynet chunk size. As long as they match,
+/// the portal can upload the chunks and free up memory while waiting for more data.
+const SKYNET_TUS_CHUNK_SIZE : u64 = (1 << 22) * 10;
+
+/// The size at which files are considered "large" and will be uploaded using the tus resumable upload protocol. This is the size of one chunk by default (40 mib). Note that this does not affect the actual size of chunks used by the protocol.
+const USE_TUS_THRESHOLD_BYTES : u64 = SKYNET_TUS_CHUNK_SIZE;
 
 #[derive(Debug)]
 pub struct UploadOptions {
@@ -129,9 +147,124 @@ pub async fn upload_data(
   let res: UploadResponse = serde_json::from_str(body_str)
     .map_err(|_| PortalResponse(body_str.to_string()))?;
 
+  // disabled since the Skynet api itself doesn't return it iike this anymore
   // let skylink = format!("{}{}", URI_SKYNET_PREFIX, res.skylink);
 
   Ok(res.skylink)
+}
+
+pub fn upload_data_tus_headers(
+  client: &SkynetClient,
+  path: &Path,
+  opt: &UploadOptions,
+) -> SkynetResult<HashMap<String, String>> {
+  let mut headers = HashMap::new();
+
+  if let Some(apikey) = &opt.api_key.clone().or(client.get_options().api_key.clone()) {
+    headers.insert("Skynet-API-Key".to_string(), apikey.clone());
+  }
+
+  if let Some(custom_user_agent) = opt.custom_user_agent.clone() {
+    headers.insert("User-Agent".to_string(), custom_user_agent);
+  }
+
+  Ok(headers)
+}
+
+pub fn upload_data_query_params(
+  client: &SkynetClient,
+  path: &Path,
+  opt: &UploadOptions,
+) -> SkynetResult<HashMap<String, String>> {
+  let filename =
+      if opt.custom_dirname.is_none() {
+        "".to_string()
+      } else {
+        if let Some(ref custom_dirname) = opt.custom_dirname {
+          custom_dirname.clone()
+        } else {
+          return Err(NoCustomDirname);
+        }
+      };
+
+  let mut query = HashMap::new();
+
+  if !filename.is_empty() {
+    query.insert("filename".into(), filename);
+  }
+
+  if let Some(ref skykey_name) = opt.skykey_name {
+    query.insert("skykeyname".into(), skykey_name.clone());
+  }
+
+  if let Some(ref skykey_id) = opt.skykey_id {
+    query.insert("skykeyid".into(), skykey_id.clone());
+  }
+
+  Ok(query)
+}
+
+pub fn upload_data_tus_uri(
+  client: &SkynetClient,
+  path: &Path,
+  opt: &UploadOptions,
+) -> SkynetResult<Uri> {
+  Ok(make_uri(
+    client.get_portal_url(),
+    "/skynet/tus".to_string(),
+    opt.api_key.clone(),
+    None,
+    upload_data_query_params(client, path, opt)?))
+}
+
+pub async fn upload_data_tus(
+  client: &SkynetClient,
+  path: &Path,
+  opt: UploadOptions,
+) -> SkynetResult<String> {
+  let mut req = reqwest::Client::builder();
+  let mut headers = upload_data_tus_headers(&client, path, &UploadOptions::default())?;
+  let headers = make_reqwest_headers(headers);
+  let uri = upload_data_tus_uri(&client, path, &UploadOptions::default())?;
+
+  req = req.default_headers(headers.clone());
+
+  let tus_client = Client::new(
+    HttpHandler::new(
+      Rc::new(req
+          .build()
+          .map_err(ReqwestError)?
+      )));
+
+  let upload_url = tus_client
+      .create(&uri.to_string(), path)
+      .await
+      .map_err(TUSError)?;
+
+  tus_client
+      .upload_with_chunk_size(&upload_url, path, SKYNET_TUS_CHUNK_SIZE as usize)
+      .await
+      .map_err(TUSError)?
+  ;
+
+  let meta = reqwest::Client::new()
+      .head(upload_url)
+      .headers(headers)
+      .header("tus-resumable", "1.0.0")
+      .send()
+      .await
+      .map_err(ReqwestError)?;
+
+  let headers = meta
+      .headers();
+
+  let skylink = headers
+      .get("skynet-skylink")
+      .expect("skylink header not found")
+      .to_str()
+      .expect("failed to parse skylink header to string");
+
+  Ok(skylink.to_string())
 }
 
 pub async fn upload_file(
@@ -143,18 +276,34 @@ pub async fn upload_file(
     return Err(NotFile);
   }
 
-  let filename = path.file_name().unwrap().to_str().unwrap().to_string();
-  let mime = if let Some(mime) = mime_guess::from_path(path).first() {
-    mime
-  } else {
-    mime::APPLICATION_OCTET_STREAM
-  };
-  let bytes = fs::read(path).map_err(FileError)?;
+  let mime = mime_guess::from_path(path)
+      .first()
+      .unwrap_or(mime::APPLICATION_OCTET_STREAM);
 
-  let mut data = HashMap::new();
-  data.insert(filename, (mime, bytes));
+  // "Large file uploads are automatically supported in skynet-js and skynet-nodejs.
+  //  Any file over 40MB will automatically use the built-in tus upload client."
+  //   - https://docs.skynetlabs.com/integrations/resumable-uploads-using-tus
+  if fs::metadata(path).map_err(FileError)?.len() >= USE_TUS_THRESHOLD_BYTES {
+    upload_data_tus(client, path, opt).await
+  }
 
-  upload_data(client, data, opt).await
+  // load data in mem and send
+  else {
+    let bytes = fs::read(path)
+        .map_err(FileError)?;
+
+    let filename = path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    let mut data = HashMap::new();
+    data.insert(filename, (mime, bytes));
+
+    upload_data(client, data, opt).await
+  }
 }
 
 pub async fn upload_directory(
@@ -199,6 +348,8 @@ pub async fn upload_directory(
 mod tests {
   use super::*;
 
+  use crate::SkynetClientOptions;
+
   #[tokio::test]
   async fn test_upload_data() {
     let client = SkynetClient::default();
@@ -221,6 +372,57 @@ mod tests {
     assert!(res.is_ok());
     let skylink = res.unwrap();
     assert!(skylink.starts_with(URI_SKYNET_PREFIX));
+  }
+
+  #[tokio::test]
+  async fn test_upload_file_tus_anon() {
+    let client = SkynetClient::default();
+    // generate 50MB file to trigger TUS upload
+    fs::write("tmp.txt", (0..USE_TUS_THRESHOLD_BYTES+10000).map(|_| "X").collect::<String>()).unwrap();
+    let path = Path::new("tmp.txt");
+    let res = upload_data_tus(&client, path, UploadOptions::default()).await;
+    fs::remove_file("tmp.txt").unwrap();
+    println!("skylink: {:?}", &res);
+    assert!(res.is_ok());
+    let skylink = res.unwrap();
+  }
+
+  // make sure to set the SKYNET_API_KEY env var for this test
+  #[tokio::test]
+  async fn test_upload_file_tus_auth() {
+    // generate 50MB file to trigger TUS upload
+    let path = Path::new("tmp.txt");
+    fs::write(path, (0..USE_TUS_THRESHOLD_BYTES+10000).map(|_| "X").collect::<String>()).unwrap();
+
+    let client = SkynetClient::new("https://skynetfree.net", SkynetClientOptions {
+      api_key: Some(std::env::var("SKYNET_API_KEY").unwrap()),
+      custom_user_agent: None
+    });
+
+    let res = upload_data_tus(&client, path, UploadOptions::default()).await;
+    fs::remove_file("tmp.txt").unwrap();
+    println!("skylink: {:?}", &res);
+    assert!(res.is_ok());
+    let skylink = res.unwrap();
+  }
+
+  // make sure to set the SKYNET_API_KEY env var for this test
+  #[tokio::test]
+  async fn test_upload_file_tus_auth_large() {
+    // generate 50MB file to trigger TUS upload
+    let path = Path::new("tmp.txt");
+    fs::write(path, (0..(700 * 1024 * 1024)).map(|_| "X").collect::<String>()).unwrap();
+
+    let client = SkynetClient::new("https://skynetfree.net", SkynetClientOptions {
+      api_key: Some(std::env::var("SKYNET_API_KEY").unwrap()),
+      custom_user_agent: None
+    });
+
+    let res = upload_data_tus(&client, path, UploadOptions::default()).await;
+    fs::remove_file("tmp.txt").unwrap();
+    println!("skylink: {:?}", &res);
+    assert!(res.is_ok());
+    let skylink = res.unwrap();
   }
 
   #[tokio::test]
